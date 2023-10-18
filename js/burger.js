7903cc6ceac9295e573b54b5c1aba691ee5c11e7
Данил Марков <markov.danil.04@mail.ru>
2023-10-18 21:33:57 +0300

    main page / burger menu

---
+++ js/burger.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const burger = document.querySelector(".menu__burger");
+//Назначение класса active при нажатии на меню-бургер
+burger.addEventListener("click", onclick);
+
+const menuList = document.querySelectorAll(".menu__link");
+
+for (const li of menuList) {
+  li.addEventListener("click", onclick);
+}
+
+function onclick() {
+  burger.classList.toggle("active");
+  if (burger.classList.contains("active")) {
+    disableScroll();
+  } else {
+    enableScroll();
+  }
+  document.querySelector(".menu__body").classList.toggle("open");
+
+  // Переключить видимость элемента "Главная"
+  const menuLinkMain = document.getElementById("menu__link-main");
+  menuLinkMain.classList.toggle("hidden");
+
+  const menuLinkContainer = document.getElementById("menu__link-container");
+  menuLinkContainer.classList.toggle("hidden");
+}
+
+function disableScroll() {
+  // Сохранить текущую позицию прокрутки
+  var scrollPosition = window.scrollY || window.pageYOffset;
+  document.documentElement.style.overflow = "hidden";
+  document.body.style.overflow = "hidden";
+  document.documentElement.scrollTop = scrollPosition;
+  document.body.scrollTop = scrollPosition;
+}
+function enableScroll() {
+  document.documentElement.style.overflow = "";
+  document.body.style.overflow = "";
+}
