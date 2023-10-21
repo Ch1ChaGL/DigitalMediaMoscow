6571119b129d2862d783301e75845441d1a3faa8
Данил Марков <markov.danil.04@mail.ru>
2023-10-21 08:48:56 +0300

    1

--- js/services.js
+++ js/services.js
@@ -1,8 +1,27 @@
-const list = document.querySelector(".services-page__list");
+const list = document.querySelector('.services-page__list');
 
-const openList = (e) => {};
+const createBlock = (dataName, element) => {
+  const servicesPage__block = document.createElement('div');
+  servicesPage__block.classList.add('services-page__block');
+  servicesPage__block.classList.add('block');
+
+  
+};
+
+const openList = e => {
+  const closestLi = e.target.closest('li');
+
+  if (closestLi && !closestLi.classList.contains('active')) {
+    closestLi.classList.add('active');
+  }
+  const dataName = closestLi.dataset.name;
+
+  createBlock(dataName, closestLi);
+};
+
+const close = e => {};
 
 for (const service of list.children) {
   console.log(service);
-  service.addEventListener("click", openList);
+  service.addEventListener('click', openList);
 }
